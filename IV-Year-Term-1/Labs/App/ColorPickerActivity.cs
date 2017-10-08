e1b2c8e918269081086a366278e7f9177815378e
Gena Falatyuk <gena.fox.gf@gmail.com>
2017-10-09 00:53:36 +0300

    Lab-1: Color Picker has been done.

---
+++ IV-Year-Term-1/Labs/App/ColorPickerActivity.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Graphics;
+using Android.OS;
+using Android.Widget;
+
+namespace App
+{
+    [Activity(Label = "@string/color_picker_label")]
+    public class ColorPickerActivity : Activity
+    {
+        private TextView colorTextVeiw;
+        private SeekBar redSeekBar;
+        private SeekBar greenSeekBar;
+        private SeekBar blueSeekBar;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.ColorPicker);
+
+            this.colorTextVeiw = FindViewById<TextView>(Resource.Id.colorTextVeiw);
+            this.redSeekBar = FindViewById<SeekBar>(Resource.Id.redSeekBar);
+            this.greenSeekBar = FindViewById<SeekBar>(Resource.Id.greenSeekBar);
+            this.blueSeekBar = FindViewById<SeekBar>(Resource.Id.blueSeekBar);
+
+            this.redSeekBar.ProgressChanged += OnSeekBarProgressChanged;
+            this.greenSeekBar.ProgressChanged += OnSeekBarProgressChanged;
+            this.blueSeekBar.ProgressChanged += OnSeekBarProgressChanged;
+
+            this.UpdateTextViewColor();
+        }
+
+        private void OnSeekBarProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+            this.UpdateTextViewColor();
+        }
+
+        private void UpdateTextViewColor()
+        {
+            int red = this.redSeekBar.Progress;
+            int green = this.greenSeekBar.Progress;
+            int blue = this.blueSeekBar.Progress;
+
+            var color = new Color(red, green, blue);
+            this.colorTextVeiw.SetBackgroundColor(color);
+        }
+    }
+}
---
+++ IV-Year-Term-1/Labs/App/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+
+namespace App
+{
+    [Activity(Label = "@string/app_name", Icon = "@drawable/labs_launcher_cast_shedow_icon", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private Button colorPickerBtn;
+        private Button calculatorBtn;
+        private Button notesBtn;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            this.colorPickerBtn = FindViewById<Button>(Resource.Id.colorPickerBtn);
+            this.calculatorBtn = FindViewById<Button>(Resource.Id.calculatorBtn);
+            this.notesBtn = FindViewById<Button>(Resource.Id.notesBtn);
+
+            colorPickerBtn.Click += (sender, e) => {
+                var activity = new Intent(this, typeof(ColorPickerActivity));
+                StartActivity(activity);
+            };
+
+            calculatorBtn.Click += (sender, e) => {
+                var activity = new Intent(this, typeof(CalculatorActivity));
+                StartActivity(activity);
+            };
+
+            notesBtn.Click += (sender, e) => {
+                var activity = new Intent(this, typeof(NotesActivity));
+                StartActivity(activity);
+            };
+        }
+    }
+}
+
