b4c170c65c0ff6f7d1f030ff89878d1476f8f59e
Gennadiy Falatiuk <gena.fox.gf@gmail.com>
2017-10-31 00:48:39 +0200

    looks like clock windget works

--- IV-Year-Term-1/Labs/NoteWidget/Resources/Resource.Designer.cs
+++ IV-Year-Term-1/Labs/NoteWidget/Resources/Resource.Designer.cs
@@ -41,11 +41,58 @@ namespace NoteWidget
 			}
 		}
 		
-		public partial class Layout
+		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Main = 2130837504;
+			public const int event_note_icon = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int noteItem_icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int volume_up_icon = 2130837506;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f060001
+			public const int widgetAnnouncementIcon = 2131099649;
+			
+			// aapt resource value: 0x7f060000
+			public const int widgetBackground = 2131099648;
+			
+			// aapt resource value: 0x7f060002
+			public const int widgetMedium = 2131099650;
+			
+			// aapt resource value: 0x7f060003
+			public const int widgetSmall = 2131099651;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int Widget = 2130903040;
 			
 			static Layout()
 			{
@@ -60,8 +107,8 @@ namespace NoteWidget
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int app_name = 2130903040;
+			// aapt resource value: 0x7f050000
+			public const int app_name = 2131034112;
 			
 			static String()
 			{
@@ -69,6 +116,22 @@ namespace NoteWidget
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Xml
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int Widget = 2130968576;
+			
+			static Xml()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Xml()
 			{
 			}
 		}
